ef8959f572b58b4643075bd7308ce36829f552ed
Jordan Rosas <jordanrosas1203@gmail.com>
2019-01-17 11:18:04 -0600

    completed react kennel practice

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class LocationList extends Component{
+  render() {
+    return (
+      <article>
+        <section>
+          <h1>Nasville North</h1>
+          <p>555 woodycrest ave</p>
+        </section>
+        <section>
+          <h1>Nashville South</h1>
+          <p>111 polk ave se</p>
+        </section>
+      </article>
+    );
+
+  }
+}
